0f3441dc02303c9cccbdf587efbbb51e366e18a1
lorello <lorenzo.salvadorini@softecspa.it>
2015-12-07 18:11:07 +0100

    added -f option to readlink to better emulate realpath behaviour

--- lib/facter/java_home.rb
+++ lib/facter/java_home.rb
@@ -2,7 +2,7 @@ Facter.add("java_home") do
   fileToTest = "/etc/alternatives/java"
   if File.exist?(fileToTest)
     setcode do
-      %x{readlink /etc/alternatives/java}.sub(/\/bin\/java/,"").chomp
+      %x{readlink -f /etc/alternatives/java}.sub(/\/bin\/java/,"").chomp
     end
   end
 end
